fb55456d218b5c3757c1cda33f4795002928e04e
Cubeur-manchot <matthieu.aubert@etu.emse.fr>
2016-05-26 16:01:45 +0200

    Et le javascript qui va avec !
    J'ai essayé de découper en plusieurs lignes sinon c'est vraiment pas beau.

---
+++ parallélisation.js
@@ -0,0 +1,24 @@
+function nav_et_scroll()
+{
+	document.write('<nav><div class="bloc_nav"><p class="en-tete-nav">MENU</p>');
+	document.write('<ul><li><a href="#">Actualités</a></li>');
+	document.write('<li><a href="methode_debutant.html">Réussir le cube !</a></li>');
+	document.write('<li><a href="#">Speedcubing 3x3</a></li>');
+	document.write('<li><a href="#">Officiels WCA</a></li>');
+	document.write('<li><a href="#">Encyclopédie</a></li>');
+	document.write('<li><a href="#">Catalogue</a></li>');
+	document.write('<li><a href="#">Entretient / Modding</a></li>');
+	document.write('<li><a href="#">Cubes Exotiques</a></li></ul></div>');
+	document.write('<div class="bloc_nav"><p class="en-tete-nav">Nous contacter :</p><ul>');
+	document.write('<li><a href="mailto:julien.combaz@gmail.com">akira1250</a></li>');
+	document.write('<li><a href="mailto:matthieu.aubert17@gmail.com">Cubeur-manchot</a></li>');
+	document.write('<li><a href="mailto:enzoguerin@gmail.com">Bushizo</a></li>');
+	document.write('<li><a href="mailto:flodedge@gmail.com">Blenderiste09</a></li></ul></div></nav>');
+	document.write('<a href="#" id="scroll_to_top"><img src="images/scroll-top.png" alt="Scroll to top"/></a>');
+}
+
+function footer()
+{
+	document.write('<footer><p>Site en développement par Cubeur-Manchot, Bushizo et Blenderiste09.</p>');
+	document.write('<p class="copyright">Copyright 2015. Pour toute demande, suggestion ou menace de mort, envoyez à l'+"'"+'adresse combaz.julien@gmail.com</p></footer>');
+}
