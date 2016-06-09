30da276ad4834001348588a3027983669ecea7dd
Cubeur-manchot <matthieu.aubert@etu.emse.fr>
2016-06-09 11:46:07 +0200

    Petite optimisation
    Je bosse encore sur le footer donc je le garde séparément, mais après on mettra tout en un.

--- parallélisation.js
+++ parallélisation.js
@@ -1,5 +1,33 @@
 function nav_et_scroll()
 {
+	document.write('<nav><section class="bloc_nav"><a href="index.html" class="en-tete-nav">MENU</a>');
+	document.write('<ul><li><a href="#">Actualités</a></li>');
+	document.write('<li><a href="methode_debutant.html">Réussir le cube !</a></li>');
+	document.write('<li><a href="#">Speedcubing 3x3</a></li>');
+	document.write('<li><a href="#">Officiels WCA</a></li>');
+	document.write('<li><a href="#">Encyclopédie</a></li>');
+	document.write('<li><a href="#">Catalogue</a></li>');
+	document.write('<li><a href="#">Entretient / Modding</a></li>');
+	document.write('<li><a href="#">Cubes Exotiques</a></li></ul></section>');
+	document.write('<section class="bloc_nav"><p class="en-tete-nav">Nous contacter :</p><ul>');
+	document.write('<li><a href="mailto:julien.combaz@gmail.com">akira1250</a></li>');
+	document.write('<li><a href="mailto:matthieu.aubert17@gmail.com">Cubeur-manchot</a></li>');
+	document.write('<li><a href="mailto:enzoguerin@gmail.com">Bushizo</a></li>');
+	document.write('<li><a href="mailto:flodedge@gmail.com">Blenderiste09</a></li></ul></sections></nav>');
+	document.write('<a href="#" id="scroll_to_top"><img src="images/scroll-top.png" alt="Scroll to top"/></a>');
+}
+
+function footer()
+{
+	document.write('<footer><p>Site en développement par Cubeur-Manchot, Bushizo et Blenderiste09.</p>');
+	document.write('<a href="plan_du_site.html">Plan du site</a>');
+	document.write('<a href="#"><img src="images/logo_facebook.png" height="17px" width="17px"/></a>');
+	document.write('<a href="#"><img src="images/logo_google_plus.png" height="17px" width="17px"/></a>');
+	document.write('<p class="copyright">Copyright 2016. Pour toute demande, suggestion ou menace de mort, envoyez à l\'adresse matthieu.aubert17@gmail.com</p></footer>');
+}
+
+function nav_scroll_banniere()
+{
 	document.write('<nav><div class="bloc_nav"><p class="en-tete-nav">MENU</p>');
 	document.write('<ul><li><a href="#">Actualités</a></li>');
 	document.write('<li><a href="methode_debutant.html">Réussir le cube !</a></li>');
@@ -15,10 +43,5 @@ function nav_et_scroll()
 	document.write('<li><a href="mailto:enzoguerin@gmail.com">Bushizo</a></li>');
 	document.write('<li><a href="mailto:flodedge@gmail.com">Blenderiste09</a></li></ul></div></nav>');
 	document.write('<a href="#" id="scroll_to_top"><img src="images/scroll-top.png" alt="Scroll to top"/></a>');
-}
-
-function footer()
-{
-	document.write('<footer><p>Site en développement par Cubeur-Manchot, Bushizo et Blenderiste09.</p>');
-	document.write('<p class="copyright">Copyright 2015. Pour toute demande, suggestion ou menace de mort, envoyez à l'+"'"+'adresse combaz.julien@gmail.com</p></footer>');
+	document.write('<header><img id="banniere"  src="images/banniere.png" alt="Bannière du site"/></header>');
 }
