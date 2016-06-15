32a2fb73422a19d0bbe9713cf6a4992353181985
Cubeur-manchot <matthieu.aubert@etu.emse.fr>
2016-06-15 22:41:42 +0200

    Grosse update de parallélisation.js
    - séparation plus propre des fonctions
    - modification des en-têtes du menu de navigation
    - ajout de liens (plan du site dans le pied de page, page des contacts, facebook et google+)
    - ajout de lien de la page facebook (eh ouais c'est nouveau, j'ai créé la page :P)
    - ajout de lien du compte google+ (eh ouais ça aussi j'ai créé :P)

--- parallélisation.js
+++ parallélisation.js
@@ -1,6 +1,6 @@
-function nav_et_scroll()
+function nav()
 {
-	document.write('<nav><section class="bloc_nav"><a href="index.html" class="en-tete-nav">MENU</a>');
+	document.write('<nav><section><a href="index.html" class="en-tete">MENU</a>');
 	document.write('<ul><li><a href="#">Actualités</a></li>');
 	document.write('<li><a href="methode_debutant.html">Réussir le cube !</a></li>');
 	document.write('<li><a href="#">Speedcubing 3x3</a></li>');
@@ -9,39 +9,40 @@ function nav_et_scroll()
 	document.write('<li><a href="#">Catalogue</a></li>');
 	document.write('<li><a href="#">Entretient / Modding</a></li>');
 	document.write('<li><a href="#">Cubes Exotiques</a></li></ul></section>');
-	document.write('<section class="bloc_nav"><p class="en-tete-nav">Nous contacter :</p><ul>');
-	document.write('<li><a href="mailto:julien.combaz@gmail.com">akira1250</a></li>');
+	document.write('<section><a href="contact.html" class="en-tete">Nous contacter :</a><ul>');
 	document.write('<li><a href="mailto:matthieu.aubert17@gmail.com">Cubeur-manchot</a></li>');
 	document.write('<li><a href="mailto:enzoguerin@gmail.com">Bushizo</a></li>');
 	document.write('<li><a href="mailto:flodedge@gmail.com">Blenderiste09</a></li></ul></sections></nav>');
+}
+
+function scroll()
+{
 	document.write('<a href="#" id="scroll_to_top"><img src="images/scroll-top.png" alt="Scroll to top"/></a>');
+}
+
+function banniere()
+{
+	document.write('<header><img id="banniere"  src="images/banniere.png" alt="Bannière du site"/></header>');
 }
 
 function footer()
 {
 	document.write('<footer><p>Site en développement par Cubeur-Manchot, Bushizo et Blenderiste09.</p>');
 	document.write('<a href="plan_du_site.html">Plan du site</a>');
-	document.write('<a href="#"><img src="images/logo_facebook.png" height="17px" width="17px"/></a>');
-	document.write('<a href="#"><img src="images/logo_google_plus.png" height="17px" width="17px"/></a>');
-	document.write('<p class="copyright">Copyright 2016. Pour toute demande, suggestion ou menace de mort, envoyez à l\'adresse matthieu.aubert17@gmail.com</p></footer>');
+	document.write('<a href="https://www.facebook.com/cubing.LaTeteDansleCube/?fref=ts"><img src="images/logo_facebook.png" class="icone"/></a>');
+	document.write('<a href="https://plus.google.com/104817566374853194180"><img src="images/logo_google_plus.png" class="icone"/></a>');
+	document.write('<p>Copyright 2016. Pour toute demande, suggestion ou menace de mort, envoyez à l\'adresse latetedanslecube@gmail.com</p></footer>');
+}
+
+function nav_scroll()
+{
+	scroll();
+	nav();
 }
 
 function nav_scroll_banniere()
 {
-	document.write('<nav><div class="bloc_nav"><p class="en-tete-nav">MENU</p>');
-	document.write('<ul><li><a href="#">Actualités</a></li>');
-	document.write('<li><a href="methode_debutant.html">Réussir le cube !</a></li>');
-	document.write('<li><a href="#">Speedcubing 3x3</a></li>');
-	document.write('<li><a href="#">Officiels WCA</a></li>');
-	document.write('<li><a href="#">Encyclopédie</a></li>');
-	document.write('<li><a href="#">Catalogue</a></li>');
-	document.write('<li><a href="#">Entretient / Modding</a></li>');
-	document.write('<li><a href="#">Cubes Exotiques</a></li></ul></div>');
-	document.write('<div class="bloc_nav"><p class="en-tete-nav">Nous contacter :</p><ul>');
-	document.write('<li><a href="mailto:julien.combaz@gmail.com">akira1250</a></li>');
-	document.write('<li><a href="mailto:matthieu.aubert17@gmail.com">Cubeur-manchot</a></li>');
-	document.write('<li><a href="mailto:enzoguerin@gmail.com">Bushizo</a></li>');
-	document.write('<li><a href="mailto:flodedge@gmail.com">Blenderiste09</a></li></ul></div></nav>');
-	document.write('<a href="#" id="scroll_to_top"><img src="images/scroll-top.png" alt="Scroll to top"/></a>');
-	document.write('<header><img id="banniere"  src="images/banniere.png" alt="Bannière du site"/></header>');
+	scroll();
+	nav();
+	banniere();
 }
