7f78db059bd909692ced43afbd81c9aebb1daaf2
Cubeur-manchot <matthieu.aubert@etu.emse.fr>
2016-07-01 00:14:29 +0200

    Modification de create_cube_2D

--- cube_generator.js
+++ cube_generator.js
@@ -34,22 +34,22 @@ function create_cube_3D(u11,u12,u13,u21,u22,u23,u31,u32,u33,f11,f12,f13,f21,f22,
 
 function create_cube_2D(u11,u12,u13,u21,u22,u23,u31,u32,u33,b1,b2,b3,l1,l2,l3,r1,r2,r3,f1,f2,f3)
 {
-	document.write('<div class="cube_2D"><div class="face arriere">');
+	document.write('<div class="cube_2D"><div class="face arriere"><div class="face_arriere">');
 		document.write('<div id="b1" class="sticker ' + b1 + '"></div>');
 		document.write('<div id="b2" class="sticker ' + b2 + '"></div>');
-		document.write('<div id="b3" class="sticker ' + b3 + '"></div></div>');
-	document.write('<div class="face gauche">');
+		document.write('<div id="b3" class="sticker ' + b3 + '"></div></div></div>');
+	document.write('<div class="face gauche"><div class="face_gauche">');
 		document.write('<div id="l1" class="sticker ' + l1 + '"></div>');
 		document.write('<div id="l2" class="sticker ' + l2 + '"></div>');
-		document.write('<div id="l3" class="sticker ' + l3 + '"></div></div>');
-	document.write('<div class="face droite">');
+		document.write('<div id="l3" class="sticker ' + l3 + '"></div></div></div>');
+	document.write('<div class="face droite"><div class="face_droite">');
 		document.write('<div id="r1" class="sticker ' + r1 + '"></div>');
 		document.write('<div id="r2" class="sticker ' + r2 + '"></div>');
-		document.write('<div id="r3" class="sticker ' + r3 + '"></div></div>');
-	document.write('<div class="face avant">');
+		document.write('<div id="r3" class="sticker ' + r3 + '"></div></div></div>');
+	document.write('<div class="face avant"><div class="face_avant">');
 		document.write('<div id="f1" class="sticker ' + f1 + '"></div>');
 		document.write('<div id="f2" class="sticker ' + f2 + '"></div>');
-		document.write('<div id="f3" class="sticker ' + f3 + '"></div></div>');
+		document.write('<div id="f3" class="sticker ' + f3 + '"></div></div></div>');
 	document.write('<div class="face haut">');
 		document.write('<div id="u11" class="sticker ' + u11 + '"></div>');
 		document.write('<div id="u12" class="sticker ' + u12 + '"></div>');
